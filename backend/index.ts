0b66331261dede2f0c0990208365d35a90b881d5
ashishverma94 <hariashish1@gmail.com>
2024-08-20 09:11:55 +0530

    Setup Bacend and database connection for project

---
+++ backend/index.ts
@@ -0,0 +1,34 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Express, NextFunction, Request, Response } from "express";
+import connectDB from "./utils/db";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+// APP
+export const app: Express = express();
+// BODY PARSER
+app.use(express.json({ limit: "50mb" }));
+// COOKIE PARSER
+app.use(cookieParser());
+// CORS [CROSS ORIGIN RESOURCE SHARING]
+app.use(cors());
+// ROUTES
+
+// TESTING API
+app.get("/", (req:Request, res:Response) =>
+  res.status(200).json({ success: true, message: "API is working" })
+);
+
+// UNKNOWN ROUTE
+app.all("*", (req:Request, res:Response, next:NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found!`);
+  next(err);
+});
+
+// SERVER
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is connected with port ${process.env.PORT}`);
+  connectDB();
+});
---
+++ backend/utils/db.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv" ;
+import mongoose from "mongoose";
+dotenv.config();
+
+const dbUrl = process.env.DB_URL || "";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbUrl).then((data) => {
+      console.log(`Database connected with ${data.connection.host}`);
+    });
+  } catch (error:any) {
+    console.log(error.message);
+    setTimeout(connectDB, 5000);
+  }
+};
+
+export default connectDB ;
